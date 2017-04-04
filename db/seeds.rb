06cade2a251dff704ba0646ae93798914aeca56e
Tetsushi Sugano <tetsushi.sugano@gmail.com>
2017-04-04 11:53:59 +0000

    user_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               name: name
+               )
+end
+
+n = 1
+while n <= 100
+  Blog.create(
+    title: "あいうえお",
+    content: "かきくけこ",
+    user_id: n
+  )
+  n = n + 1
+end
