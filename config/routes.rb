04710afcdb659a3d57e676aa8eb86a73fe00f3ec
菅野哲司 <tetsushi@tetsushisugano-MacBook-Pro.local>
2017-05-14 16:39:25 +0900

    poems views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
       post :confirm
@@ -12,8 +14,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :poems, only: [:index, :show] do
-  end
+  resources :poems, only: [:index, :show]
 
   root'top#index'
 
